b6ceaad2337492176993643cb2f4479f063fc6fb
novlihalsi <ahmadnovli@gmail.com>
2019-12-15 02:25:48 +0700

    10.menu-profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,69 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import InputBase from '@material-ui/core/InputBase';
 import {Link} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Avatar } from '@material-ui/core';
+
+
+
+function ProfilButton() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      {/* <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button> */}
+
+      <IconButton color="inherit" aria-controls="simple-menu" aria-hanspopup="true" onClick={handleClick}>
+        <AccountCircle />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        style={{marginTop:40}}
+      >
+        <MenuItem>
+        <div style={{display:'flex'}}>
+          <div style={{flex:1, marginRight:8, marginTop:5}}>
+            <Avatar src="https://miro.medium.com/fit/c/48/48/1*w1dyAyH5Pc_XysvAyd3C_Q.jpeg"/>
+            
+          </div>
+          <div style={{flex:1}}>
+            <Typography variant="subtitle1" style={{fontWeight:'bold'}}>Andy Raskin</Typography>
+            <Typography variant="caption">@AndyRaskin1902982717</Typography>
+          </div>
+
+          
+        </div>
+        </MenuItem>
+        <MenuItem style={{color:'green'}}>Become a member</MenuItem>
+        <hr/>
+        <MenuItem onClick={handleClose}>New Story</MenuItem>
+        <MenuItem onClick={handleClose}>Stories</MenuItem>
+        <MenuItem onClick={handleClose}>Stats</MenuItem>
+        <hr/>
+        <MenuItem onClick={handleClose}>Bookmark</MenuItem>
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Setting</MenuItem>
+        <MenuItem onClick={handleClose}>Help</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
 
 
 const useStyles = makeStyles(theme => ({
@@ -110,9 +173,11 @@ function ButtonAppBar() {
           {/* <Fab size="small" color="secondary">
             R
           </Fab> */}
-          <IconButton color="inherit">
+          {/* <IconButton color="inherit">
               <AccountCircle />
-            </IconButton>
+            </IconButton> */}
+
+            <ProfilButton/>
           
         </Toolbar>
       </div>
